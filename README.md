b6c53ead13ee884e5a79d593b92d605aaefc1182
vicmon810 <maokr879@student.otago.ac.nz>
2023-11-16 11:41:18 +1300

    inital commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Web_server
+Pure PHP web server
---
+++ src/Server.php
@@ -0,0 +1,10 @@
+<?php namespace ClanCats\station\PHPServer;
+
+//Server Object
+
+
+class Server {
+    protected $host = null;
+    protected $port = null;
+    protected $socket = null;
+}
