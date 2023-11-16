51dc345a1b5ce476c3327b07b9f5a711d4bb9a59
vicmon810 <maokr879@student.otago.ac.nz>
2023-11-16 13:45:12 +1300

    finsihed

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e3df2660b17c315dcd774b3d572d82c96c71fa72',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e3df2660b17c315dcd774b3d572d82c96c71fa72',
+            'dev_requirement' => false,
+        ),
+    ),
+);
